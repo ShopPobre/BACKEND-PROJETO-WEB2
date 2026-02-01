bfe5f44eb848092d101c30b6e92de596cb29a435
João Vitor da Silva Souza <129335518+Jvitorsouz@users.noreply.github.com>
2026-02-01 17:24:49 -0300

    Revert "Test/config mocha chai"

--- src/interfaces/IInventoryRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Inventory, InventoryAttributes } from "../models/Inventory";
-
-export interface IInventoryRepository {
-    create(invetoryData: Omit<InventoryAttributes, 'id'>): Promise<Inventory>;
-    findByProductId(productId: number): Promise<Inventory | null>;
-    updateQuantity(productId: number, newQuantity: number): Promise<Inventory>;
-    delete(productId: number): Promise<boolean>;
-}
